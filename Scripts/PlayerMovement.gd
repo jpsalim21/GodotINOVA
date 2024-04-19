ea0de0b63722fb52b1ba0801157ec9770a345937
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-04-18 23:12:09 -0300

    Adiciona ataque ao jogador

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+var direction
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,6 +11,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 var animacaoAtual = "Idle"
+var animationLock = false
+var attack1 = true
+var atacando = false
+var direcaoAtaque
 
 
 func _physics_process(delta):
@@ -18,20 +23,41 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_just_pressed("Atacar") and animacaoAtual != "Attack1":
+		atacar()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	direction = Input.get_axis("ui_left", "ui_right")
+	if(atacando):
+		velocity.x = direcaoAtaque * (SPEED / 2)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 	_updateAnimation()
+	
+func atacar():
+	if animationLock: return
+	if attack1:
+		attack1 = false
+		animationLock = true
+		anim.play("Attack1")
+		animacaoAtual = "Attack1"
+	else:
+		attack1 = true
+		animationLock = true
+		anim.play("Attack2")
+		animacaoAtual = "Attack2"
+	atacando = true
+	direcaoAtaque = -1 if anim.flip_h else 1
 
 func _updateAnimation():
+	if animationLock: return
 	if velocity.y != 0:
 		if animacaoAtual != "Jump":
 			animacaoAtual = "Jump"
@@ -48,4 +74,8 @@ func _updateAnimation():
 			anim.flip_h = false
 	elif velocity.x < 0:
 		anim.flip_h = true
-	
+
+func _on_animated_sprite_2d_animation_finished():
+	animationLock = false
+	atacando = false
+	_updateAnimation()
