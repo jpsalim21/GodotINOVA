820ee5da879b897197f17e9bc6e73e50691133b1
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-04-18 22:29:25 -0300

    Flash sprite ao tomar dano

---
+++ Cenas/FlashSprite.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite2D
+
+@export var flashColor : Color
+var defaultColor : Color
+@onready var time = $Timer
+
+@export var lifeController : LifeController
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	defaultColor = self_modulate
+	lifeController.tomouDano.connect(_flash)
+	pass # Replace with function body.
+
+func _flash():
+	self_modulate = flashColor
+	time.start()
+
+func _on_timer_timeout():
+	self_modulate = defaultColor
+	pass # Replace with function body.
--- Scripts/LifeController.gd
+++ Scripts/LifeController.gd
@@ -17,6 +17,7 @@ func alterarVida(qtd : int):
 	_vida = _vida + qtd
 	print("Vida: " + str(_vida))
 	if(qtd < 0):
-		tomouDano.emit()
 		if(_vida <= 0):
 			morreu.emit()
+		else:
+			tomouDano.emit()
