39ea46f540505bf03084f04ebc2efb945eff91fa
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-04-30 16:37:50 -0300

    Animações na UI

--- Scripts/UIMoedas.gd
+++ Scripts/UIMoedas.gd
@@ -1,7 +1,10 @@
 class_name Pontuacao extends Control
 
 @onready var texto : RichTextLabel = $RichTextLabel
-
+var tween : Tween
 
 func atualizarMoedas(moedas):
 	texto.text = str(moedas)
+	tween = create_tween()
+	tween.tween_property(texto, "scale", Vector2(1.5,1.5), 0.1)
+	tween.tween_property(texto, "scale", Vector2.ONE, 0.1)
